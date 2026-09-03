be510d88c8fd0a141c5cbacdef81412d9b794dae
NelsonGilberto1 <168493132+NelsonGilberto1@users.noreply.github.com>
2026-09-03 00:05:23 +0000

    Criando Rotas

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const http = require('http');
-const hostname = '0.0.0.0';//Poderia ser o 127.0.0.1 se voce quiser que fosse na maquina local, mas como é no git tem que ser o 0.0.0.0
-const port = 3000; // a pota padrao do node e a 3000.
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// o primeiro semple e o request (a requisicao) e depois tem que vir o response (a resposta).
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('banana\n');
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
 });
-//codar o cdigo e ele reflitir automaticamente sem que ele tenha que abrir e feixar.
-server.listen(port, hostname, () => {
-    console.log('Servidor rodadndo em http://${hostname}:${port}/');
+
+const Produto = [ 
+    {id: 1,  nome: "Nodebook" , preco: 6700},
+    {id: 2, nome: "Mouse", preco: 120}
+];
+
+app.get("/produtos", (req, res)=>{
+    res.status(200).json(Produto);
+
 });
+
+app.listen(port, () => {
+  console.log('Servidor rodando em http://localhost:${PORT}');
+});
+
+
 
